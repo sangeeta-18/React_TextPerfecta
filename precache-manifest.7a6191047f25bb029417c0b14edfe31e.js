76216a257a0f6b207d0658c9ff8d483a68f26e45
sangeeta-18 <sangeetak1831@gmail.com>
2023-09-26 15:49:32 +0530

    Updates

---
+++ precache-manifest.7a6191047f25bb029417c0b14edfe31e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "faa58615712da978edbe",
+    "url": "/React_TextPerfecta/static/css/main.436f119b.chunk.css"
+  },
+  {
+    "revision": "faa58615712da978edbe",
+    "url": "/React_TextPerfecta/static/js/main.dcd4a8d3.chunk.js"
+  },
+  {
+    "revision": "63aad648cf96e63ef983",
+    "url": "/React_TextPerfecta/static/js/runtime~main.e34a8349.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/React_TextPerfecta/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/React_TextPerfecta/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "e9a136763f4ac5a8c9efb9876a725c3a",
+    "url": "/React_TextPerfecta/index.html"
+  }
+];
